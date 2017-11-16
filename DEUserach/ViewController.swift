19461255c842d3add1535032b1e94345e4ad176b
D7703_08 <d7703_08@D7703-08ui-iMac.local>
2017-11-16 16:03:44 +0900

    1. storyboard -> Constraints 2. 코드로 Search Bar 만들기 3. IndexFilter

--- DEUserach/ViewController.swift
+++ DEUserach/ViewController.swift
@@ -15,9 +15,14 @@
 //                              > DEUserach.xcodeproj
 //
 //  [GitHub repository]
-//  name :
-//  addr :
+//  name : 1116_SerachBar
+//  addr : https://github.com/qudeodi/1116_SerachBar.git
 //  stat :
+//
+// [내용]
+//  1. stroyboard -> Constraints
+//  2. 코드로 SearchBar 구현
+//  3. IndexFilter
 //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 
 import UIKit
@@ -26,12 +31,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var tableV: UITableView!
 
     let arrayA = ["ac","ag","be","db","ne","db","cd","ba","df","ef"]
+    var twiceDic: [String:[String]]!
+    var twiceNames: [String]!
     var fillteredArray = [String]()
     
     var searchCon: UISearchController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let pathA = Bundle.main.path(forResource: "Twice", ofType: "json")
+        let dataA = try! Data(contentsOf: URL(fileURLWithPath: pathA!))
+        self.twiceDic = try! JSONSerialization.jsonObject(with: dataA, options: JSONSerialization.ReadingOptions.mutableLeaves) as! [String:[String]]
+        self.twiceNames = Array(twiceDic.keys)
         
         self.searchCon = UISearchController(searchResultsController: nil)
         self.searchCon.searchResultsUpdater = self
@@ -45,23 +57,47 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableV.delegate = self
 
     }
+    
+    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return self.twiceNames
+    }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.twiceNames.count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         if self.searchCon.isActive {
             return fillteredArray.count
         } else {
-            return arrayA.count
+            //return self.twiceNames.count
+            let memberName = self.twiceNames[section]
+            let specArr = self.twiceDic[memberName]
+            return specArr!.count
         }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.twiceNames[section]
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Reuse", for: indexPath)
         
-        cell.textLabel?.text = arrayA[indexPath.row]
+        if self.searchCon.isActive {
+            cell.textLabel?.text = fillteredArray[indexPath.row]
+        } else {
+            let memberName = self.twiceNames[indexPath.section]
+            let specArr = self.twiceDic[memberName]
+            
+            cell.textLabel?.text = specArr![indexPath.row]
+        }
         
-        cell.textLabel?.text = self.searchCon.isActive ? fillteredArray[indexPath.row] : arrayA[indexPath.row]
+        //cell.textLabel?.text = arrayA[indexPath.row]
+        
+        //cell.textLabel?.text = self.searchCon.isActive ? fillteredArray[indexPath.row] : self.twiceNames[indexPath.row]
         
         return cell
     }
